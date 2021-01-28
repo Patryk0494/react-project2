3c9f3f9f73985619eebd2286c77a4993aeac4710
Patryk <jpatryk0494@gmail.com>
2021-01-28 21:27:27 +0100

    project complete

--- src/Content.js
+++ src/Content.js
@@ -2,41 +2,44 @@ import React, {useState} from 'react'
 
 export default function Content() {
 
-    // function convert(amount, rate) {
-    //     let convertedVal= amount * rate;
-    //     return convertedVal
-    // }
+    function convert(amount, rate) {
+        let convertedVal= amount * rate;
+        return convertedVal
+    }
 
-    const [currency, setCurrency] = useState('')
+    const [currency, setCurrency] = useState('EUR');
+    const [amountOutput, setAmountOutput] = useState('');
+    const [amount, setAmount] = useState(0);
 
-
-    function onChange(event) {
+    function onSelectChange(event) {
         setCurrency(event.target.value)
     }
 
-    console.log(currency);
+    function onInputChange(event) {
+        setAmount(event.target.value)
+    }
 
-    // function sendRequestAndGetData() {
-    //     fetch(`http://api.nbp.pl/api/exchangerates/rates/C/${currency}/today/`).then(response => response.json())
-    //     .then( function(response) {
-    //         let rateExchange = response.rates[0].ask;
-    //         let sum = convert(amount, rateExchange);
-    //         let amountOutput = `Po przeliczniu na PLN to ${sum.toFixed(2)} zł.`;
-    //     }).catch(error => {
-    //         console.log("error: ", error);
-    //     });
-    // }
+    function sendRequestAndGetData() {
+        fetch(`http://api.nbp.pl/api/exchangerates/rates/C/${currency}/today/`).then(response => response.json())
+        .then( function(response) {
+            let rateExchange = response.rates[0].ask;
+            let sum = convert(amount, rateExchange);
+            amount ? setAmountOutput (`Po przeliczniu na PLN to ${sum.toFixed(2)} zł.`) : setAmountOutput (`Podaj kwotę do przeliczenia.`)
+        }).catch(error => {
+            console.log("error: ", error);
+        });
+    }
 
     return (
         <div className="content container">
-            <input className="amount-input" type="number" name="amount" ></input>
-            <select className="currency-select" name="currency" onChange={onChange}>  
+            <input className="amount-input" type="number" name="amount" value={amount} onChange={onInputChange}></input>
+            <select className="currency-select" name="currency" onChange={onSelectChange}>  
                 <option value="EUR">EUR</option>
                 <option value="USD">USD</option>
                 <option value="CHF">CHF</option>
             </select>
-            {/* <button class="convertBtn" onClick={convert}>Przelicz</button> */}
-            <p class="amount-output"></p>
+            <button className="convertBtn" onClick={sendRequestAndGetData}>Przelicz</button>
+            <p className="amount-output">{amountOutput}</p>
         </div>
     )
 }
